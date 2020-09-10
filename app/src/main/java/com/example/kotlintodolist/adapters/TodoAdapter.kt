ddf8cf45639d1a282358f290cec861b67316e42a
TejasMalik <tejas.malik97@gmail.com>
2020-09-11 00:03:44 +0530

    Todo V1

---
+++ app/src/main/java/com/example/kotlintodolist/adapters/TodoAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.kotlintodolist.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlintodolist.R
+import com.example.kotlintodolist.models.Todo
+
+class TodoAdapter(val todos: ArrayList<Todo>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(val item: View) : RecyclerView.ViewHolder(item) {
+        val tvTodoTask = item.findViewById<TextView>(R.id.tvTodoTask)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val li = parent?.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val itemView = li.inflate(R.layout.list_item_todo, parent, false)
+
+        return TodoViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder?.tvTodoTask.text = todos[position].task
+    }
+}
